24bece32f4bf14728b04e4198e2b1a75f1c7400c
0xAppo <acanino561@gmail.com>
2022-11-20 00:12:08 -0500

    updating contract

--- contracts/TokenFIx.sol
+++ contracts/TokenFIx.sol
@@ -1,7 +1,4 @@
-// contracts/TokenExchange.sol
 pragma solidity 0.8.10;
-
-// Import base Initializable contract
 
 // Import the IERC20 interface and and SafeMath library
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -11,39 +8,35 @@ import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 
 contract TokenFix {
     using SafeMath for uint256;
-
     using SafeERC20 for IERC20;
 
     // Contract state: exchange rate and token
     IERC20 public oldToken;
-
     IERC20 public newToken;
 
-    constructor(address _oldToken, address _newToken) {
+    constructor(IERC20 _oldToken, IERC20 _newToken) {
         oldToken = _oldToken;
         newToken = _newToken;
     }
 
     //@notice this function is to swap old tokens for new tokens at a 1:1 rate (?) 
     function swap(uint256 swapAmount) public {
-        userBalance = IERC20(oldToken).balanceOf(msg.sender);
+        uint256 userBalance = IERC20(oldToken).balanceOf(msg.sender);
 
         if (userBalance < swapAmount) {
             revert('Swap amount exceeds balance');
         }
 
-        oldToken.safeTransferFrom(oldToken, msg.sender, address.this, swapAmount);
+        oldToken.safeTransferFrom(msg.sender, address(this), swapAmount);
 
-        newToken.safeTransferFrom(newToken, address.this, msg.sender, swapAmount);
+        newToken.safeTransferFrom(address(this), msg.sender, swapAmount);
 
-    function approve(address tokenAddress) public {
-        IERC20(tokenAddress).approve(address.this, uint256(-1));
     }
 
-    function transfer() external payable {
-        uint256 tokens = msg.value.mul(rate);
-        token.transfer(msg.sender, tokens);
+    function approve(address tokenAddress) public {
+        uint256 allowance = 2**256 - 1;
 
+        IERC20(tokenAddress).approve(address(this), allowance);
     }
 
     // Initializer function (replaces constructor)
--- deploy/000_deploy_token_fix.ts
+++ deploy/000_deploy_token_fix.ts
@@ -5,11 +5,19 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy, get, save} = deployments;
 
+  const oldToken = "0x44b96717AD68d11F0D3Bf4389B788E7e7EE179Eb";
+
+  const newToken = "0x3fA6552C02bd371f2e100D5FAAD486686ce5C5c2";
+
   const {deployer} = await getNamedAccounts();
 
   const lode = await deploy('TokenFix', {
     from: deployer,
     contract: 'TokenFix',
+    args: [
+      oldToken,
+      newToken
+    ],
     log: true
   });
 
