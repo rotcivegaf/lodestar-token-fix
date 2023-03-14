5ad0e86cf001e617169518c066879d69fa32fe05
rotcivegaf <victor.fage@ripio.com>
2023-03-13 21:19:47 -0300

    up

--- contracts/TokenFIx.sol
+++ contracts/TokenFIx.sol
@@ -1,19 +1,19 @@
 pragma solidity 0.8.10;
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract TokenFix is ReentrancyGuard {
-    using SafeMath for uint256;
+contract TokenFix {
     using SafeERC20 for IERC20;
 
-    IERC20 public oldToken;
-    IERC20 public newToken;
+    error OnlyAdmin();
+
+    IERC20 public immutable oldToken;
+    IERC20 public immutable newToken;
     address public admin;
 
-    event tokenClaimed(address claimer, uint256 claimAmount);
-    event adminUpdated(address oldAdmin, address newAdmin);
+    event tokenClaimed(address indexed claimer, uint256 claimAmount);
+    event adminUpdated(address indexed oldAdmin, address indexed newAdmin);
 
     constructor(IERC20 _oldToken, IERC20 _newToken, address _admin) {
         oldToken = _oldToken;
@@ -21,20 +21,8 @@ contract TokenFix is ReentrancyGuard {
         admin = _admin;
     }
 
-    function swap(uint256 swapAmount) public nonReentrant {
-        swapInternal(swapAmount);
-    }
-
     //@notice this function is to swap old tokens for new tokens at a 1:1 rate
-    function swapInternal(uint256 swapAmount) internal {
-        uint256 userBalance = IERC20(oldToken).balanceOf(msg.sender);
-
-        if (swapAmount > userBalance) {
-            revert("Swap amount exceeds balance");
-        }
-
-        userBalance = userBalance - swapAmount;
-
+    function swap(uint256 swapAmount) external {
         oldToken.safeTransferFrom(msg.sender, address(this), swapAmount);
 
         newToken.safeTransferFrom(address(this), msg.sender, swapAmount);
@@ -44,21 +32,25 @@ contract TokenFix is ReentrancyGuard {
 
     //**ADMIN FUNCTIONS**
 
-    function _setAdmin(address _newAdmin) public {
-        require(msg.sender == admin, "Only the admin may update the admin");
+    function _setAdmin(address _newAdmin) external {
         address oldAdmin = admin;
+        if (msg.sender != oldAdmin) { revert OnlyAdmin(); }
         admin = _newAdmin;
-        emit adminUpdated(oldAdmin, admin);
+        emit adminUpdated(oldAdmin, _newAdmin);
     }
 
-    function _adminTransferAll() public {
-        require(msg.sender == admin, "Only the admin may transfer tokens out");
-        uint256 amount = newToken.balanceOf(address(this));
-        newToken.safeTransferFrom(address(this), msg.sender, amount);
+    function _adminTransferAll() external {
+        if (msg.sender != admin) { revert OnlyAdmin(); }
+        IERC20 _newToken = newToken;
+        _newToken.safeTransferFrom(
+            address(this),
+            msg.sender,
+            _newToken.balanceOf(address(this))
+        );
     }
 
-    function _adminTransfer(uint256 amount) public {
-        require(msg.sender == admin, "Only the admin may transfer tokens out");
+    function _adminTransfer(uint256 amount) external {
+        if (msg.sender != admin) { revert OnlyAdmin(); }
         newToken.safeTransferFrom(address(this), msg.sender, amount);
     }
 }
